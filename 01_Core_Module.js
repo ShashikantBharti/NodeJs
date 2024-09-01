c53a814469ef1e949bcb251032e033b2b94708f2
--replace-all <surya.indian321@gmail.com>
2024-09-02 02:12:41 +0530

    File System

--- 01_Core_Module.js
+++ 01_Core_Module.js
@@ -1,3 +1,16 @@
-const name = 'Shashikant';
+const fs = require('fs');
 
-console.log(name);
+// Creating a new file.
+fs.writeFileSync('demo.txt', 'Welcome to Node Js Tutorial');
+fs.writeFileSync('demo.txt', 'This is Shashikant, Welcome to Node JS Tutorial');
+
+// Append Data
+fs.appendFileSync('demo.txt', ' This is awesome tutorial');
+
+const buf_data = fs.readFileSync('demo.txt');
+console.log(buf_data);
+const org_data = buf_data.toString();
+console.log(org_data);
+
+// Rename File
+fs.renameSync('demo.txt', 'demo2.txt');
